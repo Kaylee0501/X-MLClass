2344553f0c51af11dd9396e747790a1688510bc7
Jinya Jiang <j9jiang@ucsd.edu>
2024-02-03 23:55:00 -0800

    update topic2vec python file, lists and alldatasets

--- topic2vec/topic2vec.py
+++ topic2vec/topic2vec.py
@@ -56,8 +56,49 @@ model = Top2Vec(documents=new_documents,
 
 
 
-# print("number of topics in total in the begining:", model.get_num_topics() )
-# topic_words, word_scores, topic_nums = model.get_topics()
+print("number of topics in total in the begining:", model.get_num_topics() )
+topic_words, word_scores, topic_nums = model.get_topics()
+# for topic in topic_nums:
+#     model.generate_topic_wordcloud(topic)
+print(topic_words)
+top_1 = []
+top_2 = []
+for inner_list in topic_words:
+    top_1.append(inner_list[0])
+    top_2.append(inner_list[0] + " " + inner_list[1])
+# Check if lengths are equal and print messages
+if len(top_1) == len(set(top_1)):
+    print("For top 1, the lengths are equal, it is " + str(len(top_1)))
+else:
+    print("For top 1, the lengths are not equal, before is "+ str(len(top_1))+", after is "+ str(len(set(top_1))))
+if len(top_2) == len(set(top_2)):
+    print("For top 2, the lengths are equal, it is " + str(len(top_2)))
+else:
+    print("For top 2, the lengths are not equal, before is "+ str(len(top_2))+", after is "+ str(len(set(top_2))))
+#print out the list without duplicsated topics
+print("top1 list is")
+print(list(set(top_1)))
+print("top2 list is" )
+print(list(set(top_2)))
+
+
+
+
+#reduced discarded, since there are redundant labels in the list
+# print("=" * 50)
+
+# # print("number of topics in total after the reduction:")
+# # #Reduce the number of topics discovered by Top2Vec.
+# # model.hierarchical_topic_reduction(num_topics=aapd_bertopic)
+# # print(model.get_num_topics(reduced = True))
+# # #top 50 words are returned in order of semantic similarity to topic with scores and unique number of every topic will be returned 
+# # topic_words, word_scores, topic_nums = model.get_topics(3)
+# # print(topic_words)
+# # sizes = [len(inner_list) for inner_list in topic_words]
+# print("number of topics in total after the reduction:")
+# model.hierarchical_topic_reduction(num_topics=aapd_bertopic)
+# print(model.get_num_topics(reduced = True))
+# topic_words, word_scores, topic_nums = model.get_topics(reduced = True)
 # # for topic in topic_nums:
 # #     model.generate_topic_wordcloud(topic)
 # print(topic_words)
@@ -79,41 +120,4 @@ model = Top2Vec(documents=new_documents,
 # print(top_1)
 # print("top2 list is" )
 # print(top_2)
-
-
-print("=" * 50)
-
-# print("number of topics in total after the reduction:")
-# #Reduce the number of topics discovered by Top2Vec.
-# model.hierarchical_topic_reduction(num_topics=aapd_bertopic)
-# print(model.get_num_topics(reduced = True))
-# #top 50 words are returned in order of semantic similarity to topic with scores and unique number of every topic will be returned 
-# topic_words, word_scores, topic_nums = model.get_topics(3)
-# print(topic_words)
-# sizes = [len(inner_list) for inner_list in topic_words]
-print("number of topics in total after the reduction:")
-model.hierarchical_topic_reduction(num_topics=aapd_bertopic)
-print(model.get_num_topics(reduced = True))
-topic_words, word_scores, topic_nums = model.get_topics(reduced = True)
-# for topic in topic_nums:
-#     model.generate_topic_wordcloud(topic)
-print(topic_words)
-top_1 = []
-top_2 = []
-for inner_list in topic_words:
-    top_1.append(inner_list[0])
-    top_2.append(inner_list[0] + " " + inner_list[1])
-# Check if lengths are equal and print messages
-if len(top_1) == len(set(top_1)):
-    print("For top 1, the lengths are equal, it is " + str(len(top_1)))
-else:
-    print("For top 1, the lengths are not equal, before is "+ str(len(top_1))+", after is "+ str(len(set(top_1))))
-if len(top_2) == len(set(top_2)):
-    print("For top 2, the lengths are equal, it is " + str(len(top_2)))
-else:
-    print("For top 2, the lengths are not equal, before is "+ str(len(top_2))+", after is "+ str(len(set(top_2))))
-print("top1 list is")
-print(top_1)
-print("top2 list is" )
-print(top_2)
 
